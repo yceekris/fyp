99d47246a2934fd6dad7fa3fdb83e8555165a791
Chineme123 <chineme2101@gmail.com>
2023-08-15 17:27:59 +0100

    last commit

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,13 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import Dashboard from './pages/Dashboard/Dashboard';
-import Registration from './pages/Registration/Registration';
-import Authentication from './pages/Authentication/Authentication';
-import LoginPage from './pages/Login/LoginPage';
-import CreateExpense from './pages/CreateExpense/CreateExpense';
-import ExpenseInputForm from './pages/ExpenseInput/ExpenseInput';
-import Budget from './pages/Budget/Budget';
-import './App.css'
+import * as PusherPushNotifications from "@pusher/push-notifications-web";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Registration from "./pages/Registration/Registration";
+import Authentication from "./pages/Authentication/Authentication";
+import LoginPage from "./pages/Login/LoginPage";
+import CreateExpense from "./pages/CreateExpense/CreateExpense";
+import ExpenseInputForm from "./pages/ExpenseInput/ExpenseInput";
+import Budget from "./pages/Budget/Budget";
+import "./App.css";
+
+const beamsClient = new PusherPushNotifications.Client({
+  instanceId: "6a4459c0-7c3d-4a3b-85fe-1781525efef8",
+});
+
+beamsClient
+  .start()
+  .then(() => beamsClient.addDeviceInterest("hello"))
+  .then(() => console.log("Successfully registered and subscribed!"))
+  .catch(console.error);
 
 const App = () => {
   return (
@@ -16,15 +28,15 @@ const App = () => {
         <Route path="/" Component={HomePage} />
         <Route path="/login" Component={LoginPage} />
         <Route path="/register" Component={Registration} />
-        <Route path='/auth' Component={Authentication} />
-        <Route path='/createres' Component={CreateExpense} />
-        <Route path='/expenseinput' Component={ExpenseInputForm} />
-        <Route path='/budget' Component={Budget} />
+        <Route path="/auth" Component={Authentication} />
+        <Route path="/createres" Component={CreateExpense} />
+        <Route path="/expenseinput" Component={ExpenseInputForm} />
+        <Route path="/budget" Component={Budget} />
         <Route path="/dashboard" Component={Dashboard} />
         {/* Add more routes for other pages */}
       </Routes>
-  </Router>
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- Client/src/pages/ExpenseInput/ExpenseInput.tsx
+++ Client/src/pages/ExpenseInput/ExpenseInput.tsx
@@ -1,25 +1,21 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import FormContainer from "../../components/FormContainer/FormContainer";
 import "./ExpenseInput.css";
 import Nav from "../../components/Nav/Nav";
 
 interface ExpenseInput {
   amount: number;
-  category: string;
-  description?: string;
-  paymentMethod?: string;
+  date: string;
 }
 
 const ExpenseInputForm: React.FC = () => {
   const [expense, setExpense] = useState<ExpenseInput>({
     amount: 0,
-    category: "",
-    description: "",
-    paymentMethod: "",
+    date: "",
   });
 
   const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+    event: React.ChangeEvent<HTMLInputElement>
   ) => {
     const { name, value } = event.target;
     setExpense((prevExpense) => ({
@@ -35,15 +31,14 @@ const ExpenseInputForm: React.FC = () => {
     // Reset the form after submission
     setExpense({
       amount: 0,
-      category: "",
-      description: "",
-      paymentMethod: "",
+      date: "",
     });
   };
 
   return (
     <div className="expense-input">
       <Nav />
+      <h3>Enter Your Total Expenses For Today</h3>
       <FormContainer
         container_class="form-container"
         children={
@@ -59,44 +54,19 @@ const ExpenseInputForm: React.FC = () => {
                 required
               />
             </div>
+           
             <div className="box">
-              <label htmlFor="category">Category:</label>
+              <label htmlFor="date">Day:</label>
               <input
-                type="text"
-                id="category"
-                name="category"
-                value={expense.category}
+                type="date"
+                id="date"
+                name="date"
+                value={expense.date}
                 onChange={handleInputChange}
                 required
               />
             </div>
-            <div className="box">
-              <label htmlFor="description">Description:</label>
-              <textarea
-                id="description"
-                name="description"
-                value={expense.description}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="box">
-              <label htmlFor="paymentMethod">Payment Method:</label>
-              <select
-                id="paymentMethod"
-                name="paymentMethod"
-                value={expense.paymentMethod}
-                onChange={handleInputChange}
-                required
-              >
-                <option value="">Select Payment Method</option>
-                <option value="Card">Card</option>
-                <option value="Cash">Cash</option>
-                <option value="Bank Transfer">Bank Transfer</option>
-              </select>
-            </div>
-            <div className="buttons">
-              <button type="submit">Submit</button>
-            </div>
+            
           </form>
         }
       />
